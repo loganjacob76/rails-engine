fe5d812056ebbc9440dd9bb6181e993bd3c3e7d0
loganjacob76 <nagolisback@gmail.com>
2021-05-26 16:17:27 -0600

    Add pagination methods

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,15 @@
 class API::APIController < ActionController::Base
+  def paginate(query)
+    query.limit(page_size).offset(page * page_size)
+  end
+
+  def page
+    [
+      params.fetch(:page, 1).to_i, 1
+    ].max - 1
+  end
+  
+  def page_size
+    params.fetch(:per_page, 20).to_i
+  end
 end
