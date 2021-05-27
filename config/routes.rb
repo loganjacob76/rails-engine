c371d97163f0e56ccb4f61e85170d13f2353c180
loganjacob76 <nagolisback@gmail.com>
2021-05-27 15:28:20 -0600

    Add items controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, controller: :merchant_items, only: :index
       end
+      resources :items, except: :new
     end
   end
 end
