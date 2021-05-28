a5e49705f316b54cafb89a047f2f09e7be672519
loganjacob76 <nagolisback@gmail.com>
2021-05-27 20:21:35 -0600

    Update revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find', to: 'merchants#find'
+      get 'revenue/merchants', to: 'merchants#revenue'
 
       get 'items/find_all', to: 'items#find_all'
 
